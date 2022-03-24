048108ac1c2df078fc6bd1d4c1487e54ab608ef1
Bryan Cardenas Amariles <bcardena@bancolombia.com.co>
2022-03-23 21:13:32 -0500

    primer commit con estructura gradle

---
+++ src/main/java/co/com/edu/udea/oldwavespringapi/OldwaveSpringApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.edu.udea.oldwavespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldwaveSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldwaveSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/edu/udea/oldwavespringapi/OldwaveSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.edu.udea.oldwavespringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldwaveSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
